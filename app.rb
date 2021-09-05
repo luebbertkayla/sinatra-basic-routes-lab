f7991fd5888dd6cf27e4ef0f4ef33449a0832cc6
Kayla <luebbertkayla@gmail.com>
2021-09-04 22:17:19 -0400

    commit tree a347261a6d409b2509b8ae2e8f5955df8ace77df

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Kayla"
+    end 
+
+    get '/hometown' do
+        "My hometown is Saginaw"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is 'Two Time'"
+    end 
 end
